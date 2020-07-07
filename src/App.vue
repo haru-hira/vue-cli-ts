e6c5ba0ec93399a28246de62b1891d7a4f91bba7
admin_haru <moto55.haru13@gmail.com>
2020-07-07 17:52:23 +0900

    added for emit sample.

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
+    <LikeNumber :total-number="number" @my-click="incrementNumber" @my-click2="incrementNumber2"></LikeNumber>
     <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
   </div>
 </template>
@@ -8,15 +9,33 @@
 <script lang="ts">
 import Vue from 'vue';
 import HelloWorld from './components/HelloWorld.vue';
+import LikeNumber from './components/LikeNumber.vue';
 
 export default Vue.extend({
   name: 'App',
   data() {
     return {
+      number: 14
     };
   },
   components: {
-    HelloWorld
+    HelloWorld,
+    LikeNumber
+  },
+  methods: {
+    incrementNumber(num: number, value1: string, value2: string, value3: string) {
+      console.log(value1);
+      console.log(value2);
+      console.log(value3);
+      this.number = num;
+    },
+    incrementNumber2(value: {
+        hoge: string;
+        piyo: string;
+        fuga: string;
+      }) {
+      console.log(value);
+    }
   }
 });
 </script>
---
+++ src/components/LikeNumber.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <p>いいね({{ halfNumber }})</p>
+    <button @click="increment">+1</button>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({
+  name: 'LikeNumber',
+  props: { 
+    totalNumber: {
+      type: Number
+    }
+  },
+  computed: {
+    halfNumber(): number {
+      return this.totalNumber;
+    }
+  },
+  methods: {
+    increment() {
+      const hoge = 1;
+      const piyo = 2;
+      const fuga = 3;
+      this.$emit("my-click", this.totalNumber + 1, hoge, piyo, fuga);
+      this.$emit("my-click2", {
+        hoge: hoge,
+        piyo: piyo,
+        fuga: fuga
+      });
+    }
+  }
+});
+</script>
+
+<style scoped>
+div {
+  border: 1px solid red;
+}
+</style>
