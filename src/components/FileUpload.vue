2b4dad6aaeac1f646c942021a60aa3dba293d60f
admin_haru <moto55.haru13@gmail.com>
2020-07-12 15:20:15 +0900

    added split upload logic.

--- src/components/FileUpload.vue
+++ src/components/FileUpload.vue
@@ -1,7 +1,8 @@
 <template>
   <div>
     <input type="file" accept=".jpg,.jpeg,.png,.gif,.pdf" ref="fileSelector"/>
-    <input type="submit" value="送信" @click="submit" ref="submit"/> 
+    <input type="submit" value="送信" @click="submitUpload"/>
+    <input type="submit" value="送信(分割)" @click="submitSplitUpload"/>
   </div>
 </template>
 
@@ -12,7 +13,7 @@ import axios from 'axios';
 export default Vue.extend({
   name: 'FileUpload',
   methods: {
-    submit() {
+    submitUpload() {
       // ファイル要素から、選択されたファイルを取得する
       const files = (this.$refs.fileSelector as InstanceType<typeof HTMLInputElement>).files;
 
@@ -36,42 +37,161 @@ export default Vue.extend({
             headers: {
               'Content-Type': file.type,
             }
-          })
-          .then(() => {
+          }).then(() => {
             axios.put('http://localhost:80/document/complete-upload/' + res.data.id, {
               isSuccess: true,
               fileName: file.name,
               contentType: file.type
-            })
-            .then(() => {
+            }).then(() => {
               alert("upload: success!");
               return true;
-            })
-            .catch((e2) => {
+            }).catch((e2) => {
               // アップロードできたが通知に失敗した場合
               alert(e2);
               return false
             });
-          })
-          .catch((e2) => {
+          }).catch((e2) => {
             axios.put('http://localhost:80/document/complete-upload/' + res.data.id, {
               isSuccess: false
-            })
-            .then(() => {
+            }).then(() => {
               alert(e2);
               return false;
-            })
-            .catch((e3) => {
+            }).catch((e3) => {
               alert(e3);
               return false
             });
           });
-        })
-        .catch((e1) => {
+        }).catch((e1) => {
           alert(e1);
           return false
         });
       }
+    },
+    submitSplitUpload() {
+      console.log("File: " + window.File)
+      console.log("FileReader: " + window.FileReader)
+      console.log("FileList: " + window.FileList)
+      console.log("Blob: " + window.Blob)
+
+      // ファイル要素から、選択されたファイルを取得する
+      const files = (this.$refs.fileSelector as InstanceType<typeof HTMLInputElement>).files;
+
+      // ファイルが選択されていなかったら終了
+      if (!files || files.length === 0) {
+        console.log("ファイルが選択されていません");
+        return false;
+      }
+
+      // 1つ目のファイルを取得する(非multiple)
+      const file = files[0];
+
+      // 前提1: Gitプロジェクト"nest-typeorm"をローカルで起動
+      axios.get('http://localhost:80/document/init-split-upload')
+      .then((res) => {
+        console.log("log: 1.success");
+        const uploadId: string = res.data.uploadId;
+        const key: string = res.data.key;
+        console.log("log: 1.uploadId:" + uploadId);
+        console.log("log: 1.key:" + key);
+        const partSize = 1024 * 1024 * 5; // 5MB/chunk
+        const allSize = file.size;
+        
+        this.sendDataLoop(allSize, partSize, file, uploadId, key)
+        .then((multipartMap) => {
+          axios.put('http://localhost:80/document/complete-split-upload/0', {
+            uploadId: uploadId,
+            key: key,
+            multipartUpload: multipartMap
+          }).then(() => {
+            console.log("log: 3.success");
+            alert("split upload success!!");
+            return true;
+          }).catch((e3) => {
+            alert(e3);
+            return false;
+          });
+        })
+        .catch((e2) => {
+          alert(e2);
+          return false;
+        });
+      }).catch((e1) => {
+        alert(e1);
+        return false
+      });
+    },
+    sendDataLoop(allSize: number, partSize: number, file: File, uploadId: string, key: string) {
+      return new Promise((resolve: (value?: { Parts: { ETag: string; PartNumber: number }[] }) => void) => {
+        const getPageTitle = async (allSize: number, partSize: number, file: File, uploadId: string, key: string) => {
+          const multipartMap: {
+            Parts: { ETag: string; PartNumber: number }[];
+          } = { Parts: [] }
+          let partNum = 0;
+          for (let rangeStart = 0; rangeStart < allSize; rangeStart += partSize) {
+            partNum++;
+            const end = Math.min(rangeStart + partSize, allSize);
+            
+            await this.getSendData(file, rangeStart, end, partNum)
+            .then(async (value) => {
+              const progress = end / file.size;
+              console.log(`今,${progress * 100}%だよ`);
+
+              const uploadParams = new URLSearchParams();
+              uploadParams.append('uploadId', uploadId);
+              uploadParams.append('key', key);
+              uploadParams.append('partNum', String(value.partNum));
+              uploadParams.append('sendData', String(value.byte));
+              await axios.post('http://localhost:80/document/split-upload', uploadParams)
+              .then((res) => {
+                console.log("log: 2.success/ETag:" + res.data.ETag);
+                multipartMap.Parts.push({
+                  ETag: res.data.ETag,
+                  PartNumber: res.data.PartNumber
+                });
+                return;
+              }).catch((e3) => {
+                alert(e3);
+                return false;
+              });
+            })
+            .catch((e2) => {
+              alert(e2);
+              return false;
+            });
+          }
+          console.log("log: 2*.sendDataLoop inner finish!");
+          return multipartMap;
+        }
+        getPageTitle(allSize, partSize, file, uploadId, key)
+        .then((multipartMap) => {
+          console.log("log: 2*.sendDataLoop finish!");
+          resolve(multipartMap);
+        })
+      })
+    },
+    getSendData(file: File, rangeStart: number, end: number, partNum: number) {
+      return new Promise((resolve: (value?: { byte: Uint8Array; partNum: number }) => void) => {
+        console.log("log: getSendData()");
+        const fileReader =  new FileReader();
+
+        fileReader.onload = (event) => {
+          console.log("log: getSendData().onload");
+          const data = event.target?.result;
+          let byte: Uint8Array;
+          if (data) {
+            if (data instanceof ArrayBuffer) {
+              byte = new Uint8Array(data);
+              resolve({ byte: byte, partNum: partNum });
+            } else {
+              byte = (new TextEncoder).encode(data);
+              resolve({ byte: byte, partNum: partNum });
+            }
+          }
+          fileReader.abort();
+        };
+        const splitedBlob = file.slice(rangeStart , end);
+        fileReader.readAsArrayBuffer(splitedBlob);
+      })
     }
   }
 });
