216a54705ee65d5d5957fb991cae5a7361a452ba
admin_haru <moto55.haru13@gmail.com>
2020-07-26 14:59:33 +0900

    used v-click-outside

--- src/components/VuetifySample.vue
+++ src/components/VuetifySample.vue
@@ -2,7 +2,20 @@
   <v-container class="grey lighten-2">
     <v-row class="grey lighten-3" align="center">
       <v-col class="grey lighten-4">
-        <v-card outlined>vuetify sample</v-card>
+        <v-card
+          v-click-outside="onClickOutside"
+          :color="active ? 'primary' : undefined"
+          :dark="active"
+          class="mx-auto"
+          height="128"
+          rounded="xl"
+          width="128"
+          @click="active = true"
+        >
+          <div class="text-h6 text-md-h6 fill-height d-flex align-center justify-center">
+            {{ active ? 'Click Outside' : 'Click Me' }}
+          </div>
+        </v-card>
       </v-col>
     </v-row>
     <v-divider></v-divider>
@@ -14,11 +27,13 @@ import Vue from 'vue';
 
 export default Vue.extend({
   name: 'VuetifySample',
-  props: { 
-    //
-  },
+  data: () => ({
+    active: false,
+  }),
   methods: {
-    //
+    onClickOutside () {
+      this.active = false
+    },
   }
 });
 </script>
