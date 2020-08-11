1538e5a8b275b68f774f9ed214b6acb230dcf53e
admin_haru <moto55.haru13@gmail.com>
2020-08-11 20:49:12 +0900

    added v-carousel.

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -50,6 +50,32 @@
             </v-btn>
           </v-speed-dial>
         </v-row>
+        <v-row>
+          <v-carousel
+            cycle
+            height="400"
+            hide-delimiter-background
+            show-arrows-on-hover
+          >
+            <v-carousel-item
+              v-for="(slide, i) in slides"
+              :key="i"
+            >
+              <v-sheet
+                :color="slideColors[i]"
+                height="100%"
+              >
+                <v-row
+                  class="fill-height"
+                  align="center"
+                  justify="center"
+                >
+                  <div class="display-3">{{ slide }} Slide</div>
+                </v-row>
+              </v-sheet>
+            </v-carousel-item>
+          </v-carousel>
+        </v-row>
       </v-col>
       <v-col cols="12">
         <FileUpload></FileUpload>
@@ -185,6 +211,20 @@
         },
       ],
       fab: false,
+      slideColors: [
+        'indigo',
+        'warning',
+        'pink darken-2',
+        'red lighten-1',
+        'deep-purple accent-4',
+      ],
+      slides: [
+        'First',
+        'Second',
+        'Third',
+        'Fourth',
+        'Fifth',
+      ],
       ecosystem: [
         {
           text: 'vuetify-loader',
