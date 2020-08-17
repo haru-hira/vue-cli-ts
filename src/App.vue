b7a35557a4b18495b76c8270535b51301c50d5e5
admin_haru <moto55.haru13@gmail.com>
2020-08-17 23:38:45 +0900

    fixed margin for v-divider.

--- src/App.vue
+++ src/App.vue
@@ -107,34 +107,22 @@
       dark
       shift
     >
-      <v-divider
-        class="mx-4"
-        vertical
-      ></v-divider>
+      <v-divider vertical></v-divider>
       <v-btn value="recent">
         <span>Recent</span>
         <v-icon>mdi-history</v-icon>
       </v-btn>
-      <v-divider
-        class="mx-4"
-        vertical
-      ></v-divider>
+      <v-divider vertical></v-divider>
       <v-btn value="favorites">
         <span>Favorites</span>
         <v-icon>mdi-heart</v-icon>
       </v-btn>
-      <v-divider
-        class="mx-4"
-        vertical
-      ></v-divider>
+      <v-divider vertical></v-divider>
       <v-btn value="nearby">
         <span>Nearby</span>
         <v-icon>mdi-map-marker</v-icon>
       </v-btn>
-      <v-divider
-        class="mx-4"
-        vertical
-      ></v-divider>
+      <v-divider vertical></v-divider>
     </v-bottom-navigation>
   </v-app>
 </template>
