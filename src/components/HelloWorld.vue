1e1a006d4261e702779154548bd3a3959b41ba38
admin_haru <moto55.haru13@gmail.com>
2020-08-19 22:24:47 +0900

    added v-expansion-panels.

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -149,6 +149,19 @@
         </v-row>
       </v-col>
       <v-col cols="12">
+        <v-expansion-panels>
+          <v-expansion-panel
+            v-for="(item, i) in 3"
+            :key="i"
+          >
+            <v-expansion-panel-header>Item</v-expansion-panel-header>
+            <v-expansion-panel-content>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+            </v-expansion-panel-content>
+          </v-expansion-panel>
+        </v-expansion-panels>
+      </v-col>
+      <v-col cols="12">
         <v-chip
           class="ma-2"
           color="indigo"
