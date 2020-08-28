05dc80028857755bedea5e3b93c334bea6dd0770
admin_haru <moto55.haru13@gmail.com>
2020-08-28 23:35:23 +0900

    added v-footer.

--- src/App.vue
+++ src/App.vue
@@ -124,6 +124,19 @@
       </v-btn>
       <v-divider vertical></v-divider>
     </v-bottom-navigation>
+    <v-card height="150">
+      <v-footer
+        absolute
+        class="font-weight-medium"
+      >
+        <v-col
+          class="text-center"
+          cols="12"
+        >
+          {{ new Date().getFullYear() }} — <strong>Vuetify</strong>
+        </v-col>
+      </v-footer>
+    </v-card>
   </v-app>
 </template>
 
